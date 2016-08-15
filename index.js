05a5d63aa8909eb8ed0b8082ec74837ebeec3369
Jody Jacobus Geers <y2k_geers@hotmail.com>
2016-08-15 10:40:41 +0200

    Update index.js
    Refactored.

--- index.js
+++ index.js
@@ -3,17 +3,22 @@
   "description": "ikkle client-side store.",
 */
 
-var Store = require('./_store/Store.js')
-  , Model = require('./models/Model.js')
-  , Provider = require('./provider/Provider.js')
-  ;
+
+var Store = require( './_store/Store.js' ),
+    Model = require( './models/Model.js' ),
+    Provider = require( './provider/Provider.js' )
+;
+
 
 // visible API - Functionality
-function createStore (args) {
-  var models = (args)?  args.models : Model;
-  var provider = (args)?  args.provider : Provider;
+function createStore( args ) {
+  
+  var models = ( args )?  args.models : Model;
+  var provider = ( args )?  args.provider : Provider;
 
   return new Store({ models: models, provider: provider });
+  
 };
+
 
 module.exports = createStore();
