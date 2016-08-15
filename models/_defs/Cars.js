d7267c92fd88724ad95d9c056f3ce7d801226063
Jody Jacobus Geers <y2k_geers@hotmail.com>
2016-08-15 10:47:33 +0200

    Update Cars.js

--- models/_defs/Cars.js
+++ models/_defs/Cars.js
@@ -3,15 +3,23 @@
   "description": "ikkle client-side store.",
 */
 
-function Cars(args) {
+/**
+ * Cars model difinition
+ * @return {cars} instance
+ * @access public
+ */
+function Cars( args ) {
+    
     this.meta = {
         storeName: 'Cars',
         storeLocation: 'server'
     };
+    
     this.id = (args)? args.id : undefined;
     this.year = (args)? args.year : undefined;
     this.make = (args)? args.make : undefined;
     this.model = (args)? args.model : undefined;
+
 };
 
 module.exports = Cars;
